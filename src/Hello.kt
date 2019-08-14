7e5bddf5d33adb942c9d18135b987ce99987bbfc
Pavel Batov <Pbatov@aplana.com>
2019-08-14 16:51:22 +0300

    4th step

---
+++ src/Hello.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("********************************\n"
+            + "\t THIS IS 4th task\t\n"
+            + "*******************************\n\n"
+            + "\t- Say me, what is your name?\t\r\n\n");
+
+    println("########################\n"
+            + "\t- HELLO ${readLine()}\n"
+    )
+
+}
